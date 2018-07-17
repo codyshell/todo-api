726429bc95a0ecd955a263493b36c4a689745b55
Cody Shell <codyshell@mail.com>
2018-07-17 19:57:22 +0530

    Added /todos and /todos:id

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Lunch meet',
+	completed: false
+}, {
+	id: 2,
+	description: 'Shopping',
+	completed: false
+}, {
+	id: 3,
+	description: 'Pet supplies',
+	completed: true
+}];
 
 app.get('/', function(req, res){
 	res.send('To do API root');
 });
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			matchedTodo = todo;
+		}
+	});
+	if(matchedTodo){
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+	res.send('Asking for todo id: ' + req.params.id);
+}); 
 
 app.listen(PORT, function(){
 	console.log('Express running at port:' + PORT);
