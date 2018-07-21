a61e8ddd41065045b3b488c22b13e623cefbc6a7
Cody Shell <codyshell@mail.com>
2018-07-21 06:14:13 +0530

    Refactor API with underscore

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
+var _=require('underscore');
+
 app.use(bodyParser.json());
 /* var todos = [{
 	id: 1,
@@ -26,8 +28,8 @@ app.get('/', function(req, res){
 app.get('/todos', function(req, res){
 	res.json(todos);
 });
-
-app.get('/todos/:id', function(req, res){
+//GET /todos/:id without using underscorejs
+/* app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id);
 	var matchedTodo;
 	todos.forEach(function(todo){
@@ -41,16 +43,37 @@ app.get('/todos/:id', function(req, res){
 		res.status(404).send();
 	}
 	res.send('Asking for todo id: ' + req.params.id);
-}); 
-
-app.post('/todos', function(req, res){
+}); */ 
+//GET /todos/:id using underscorejs
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+		if(matchedTodo){
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+})
+//POST /todos without using underscorejs
+/* app.post('/todos', function(req, res){
 	var body = req.body;
 	body.id = todoNextId++;
 	todos.push(body);
 	//console.log(body);
 	res.json(body);
+}); */
+//POST /todos using underscorejs for validations
+app.post('/todos', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+	todos.push(body);
+	//console.log(body);
+	res.json(body);
 });
-
 app.listen(PORT, function(){
 	console.log('Express running at port:' + PORT);
 });
