ee554a644c231f79454050eca5f9fcb9c9618ffe
Cody Shell <codyshell@mail.com>
2018-07-23 09:05:15 +0530

    New file variable playground

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+/* var person = {
+	name: "Alpha",
+	age: 30
+};
+
+function updatePerson(obj){
+	// obj = {
+		// name: "Alpha",
+		// age: 25
+	// };
+	obj.age = 25;
+}
+
+updatePerson(person);
+console.log(person); */
+
+var grades = [14, 24];
+
+function addGrades(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+	// gradesArr = [36, 13];
+}
+
+addGrades(grades);
+console.log(grades);
